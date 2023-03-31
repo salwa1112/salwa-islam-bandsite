29297573d60f5c0bd51b9443cdc38d220dfc8fce
Salwa Islam <salwasaki1112@gmail.com>
2023-03-31 01:47:19 -0400

    Removed static list element(comment)

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,7 +3,7 @@ const historicalComments = [
         img_src: "./assets/images/default.jpg",
         author_name: "Connor Walton",
         post_date: "02/17/2021",
-        description: "This is art. This is inexplicable magic\
+        comment: "This is art. This is inexplicable magic\
                 expressed in the purest way, everything\
                 that makes up this majestic work\
                 deserves reverence. Let us appreciate this for what it is and what it contains."
@@ -12,7 +12,7 @@ const historicalComments = [
         img_src: "./assets/images/default.jpg",
         author_name: "Emilie Beach",
         post_date: "01/09/2021",
-        description: "I feel blessed to have seen them in\
+        comment: "I feel blessed to have seen them in\
         person. What a show! They were just\
         perfection. If there was one day of my\
         life I could relive, this would be it. What\
@@ -22,7 +22,7 @@ const historicalComments = [
         img_src: "./assets/images/default.jpg",
         author_name: "Miles Acosta",
         post_date: "12/20/2020",
-        description: "I can t stop listening. Every time I hear\
+        comment: "I can t stop listening. Every time I hear\
         one of their songs - the vocals - it gives\
         me goosebumps. Shivers straight down\
         my spine. What a beautiful expression of\
@@ -34,13 +34,13 @@ document.addEventListener('DOMContentLoaded', () => {
     //load Previous Comments
     loadHistoricalComments();
 
-    const commentButton = document.querySelector('.comments-section__btn');
+    const commentButton = document.querySelector('.comments-section-form-content__btn');
     commentButton.addEventListener('click', (e) => {
         //Stop propagation on button click
         e.stopPropagation();
     });
 
-    const commentSubmissionForm = document.querySelector('.comments-section__frm');
+    const commentSubmissionForm = document.querySelector('.comments-section-form-content__frm');
     commentSubmissionForm.addEventListener('submit', onCommentSubmitted);
 });
 
@@ -61,14 +61,14 @@ function loadHistoricalComments(){
     });
 
     //Add top border after adding historical comments
-    const ulComments = document.querySelector('.comments-section__list');
+    const topComment = document.querySelector('.comments-section-history');
     const commentsSection = document.querySelector('.comments-section');
-    const divider = getElement('hr','comments-section__divider');
-    commentsSection.insertBefore(divider, ulComments);
+    const divider = getElement('hr','comments-section-history__divider');
+    commentsSection.insertBefore(divider, topComment);
 }
 
 function onCommentSubmitted(event){
-//  Prevent default submit action
+//  Prevent default submit
     event.preventDefault();
 
     const currentDate = new Date();
@@ -77,7 +77,7 @@ function onCommentSubmitted(event){
         author_name: event.target.name.value,
         post_date: `${(currentDate.getMonth()+1).toString().padStart(2,'0')}/
             ${currentDate.getDate().toString().padStart(2,'0')}/${currentDate.getFullYear()}`,
-        description: event.target.comment.value
+        comment: event.target.comment.value
     };
 
     //Add comment
@@ -91,56 +91,48 @@ function addComment(comment){
     if(typeof(comment) != "object")
         return;
 
-    let ulComments = document.querySelector('.comments-section__list');
+    const commentsSectionHistoryHeaderDiv = getElement('div','comments-section-history-header');  
+    const commentAuthorNameHeader = getElement('h3',"comments-section-history-header__authorname");
+    const commentPostDate = getElement('p',"comments-section-history-header__postdate");
 
-    if(!ulComments){
-        //No previous comments list found, create a new one
-        ulComments = getElement('ul', 'comments-section__list');
-    }
+    const commentText = getElement('p', 'comments-section-history-info__comment');
+    const commentDetailsRoot = getElement('div','comments-section-history-info-posted_comment');
 
-    const listItem = getElement('li', 'comments-section__list-item');
-    
-    const listItemContentRoot = getElement('div', 'comments-section__list-item-content');
-    const commentsSectionImage = getElement('img', 'comments-section__img');
-    const commentContainer = getElement('div', 'comments-section__posted-comment');
-    const commentContainerChild_Header = getElement('div', 'comments-section__list-item-header');
-    const commentHeaderChild_Name = getElement('h3', 'comments-section__list-item-author-name');
-    const commentHeaderChild_PostDate = getElement('p', 'comments-section__list-item-post-date');
-    const commentContinerChild_Description = getElement('p', 'comments-section__list-item-description');
-    const commentDivider = getElement('hr', 'comments-section__divider');
-    
-    //Append  header items to the header container
-    commentContainerChild_Header.appendChild(commentHeaderChild_Name);
-    commentContainerChild_Header.appendChild(commentHeaderChild_PostDate);
+    const commentsHistoryInfoRoot = getElement('div', "comments-section-history-info");
+    const authorImageElement = getElement('img', "comments-section-history-info__img");
 
-    //Append header and comment description to the comment continer
-    commentContainer.appendChild(commentContainerChild_Header);
-    commentContainer.appendChild(commentContinerChild_Description);
+    const commentHistoryRoot = getElement('div', 'comments-section-history');
+    const divider = getElement('hr',"comments-section-history__divider");
 
-    //Append image and the comment container and the diveder to the list item class
-    listItemContentRoot.appendChild(commentsSectionImage);
-    listItemContentRoot.appendChild(commentContainer);
-   
-    //Add to the list item
-    listItem.appendChild(listItemContentRoot);
-    listItem.appendChild(commentDivider);
+    commentsSectionHistoryHeaderDiv.appendChild(commentAuthorNameHeader);
+    commentsSectionHistoryHeaderDiv.appendChild(commentPostDate);
 
-    //Add data to the items
-    commentHeaderChild_Name.textContent = comment.author_name;
-    commentHeaderChild_PostDate.textContent = comment.post_date;
-    commentContinerChild_Description.textContent = comment.description;
-    commentsSectionImage.src = comment.img_src;
-    commentsSectionImage.alt = `profile photo of ${comment.author_name}`;
+    commentDetailsRoot.appendChild(commentsSectionHistoryHeaderDiv);
+    commentDetailsRoot.appendChild(commentText);
 
 
-    //Add comment to list
-    ulComments.insertBefore(listItem, ulComments.firstChild);
+    commentsHistoryInfoRoot.appendChild(authorImageElement);
+    commentsHistoryInfoRoot.appendChild(commentDetailsRoot);
+    commentHistoryRoot.appendChild(commentsHistoryInfoRoot);
+
+    //Add comment values
+    commentText.textContent = comment.comment;
+    commentPostDate.textContent = comment.post_date;
+    commentAuthorNameHeader.textContent = comment.author_name;
+    authorImageElement.src = comment.img_src;
+    authorImageElement.alt = `profile image of ${comment.author_name}`;
+
+    //Add to root comment section
     const commentSection = document.querySelector('.comments-section');
-    commentSection.appendChild(ulComments);
+    const firstComment= document.querySelector('.comments-section-history');
+    commentSection.insertBefore(commentHistoryRoot,firstComment);
+
+    //Add divider after each comment
+    commentSection.insertBefore(divider, firstComment);
 }
 
 function clearForm() {
-    const form = document.querySelector('.comments-section__frm');
+    const form = document.querySelector('.comments-section-form-content__frm');
     form.reset();
 }
 
